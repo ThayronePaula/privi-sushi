206fc966febdd8fce0cf395458dceb85c1540d50
Lidiane Faria <lidianerdefaria@gmail.com>
2021-09-15 16:38:39 -0300

    feat: finished modal screen characteristics

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,19 +8,11 @@ import {
   InputGroup,
   InputLeftElement,
   Button,
-  OrderedList,
   Avatar
 } from '@chakra-ui/react'
-import { EmailIcon, SearchIcon, SmallCloseIcon } from '@chakra-ui/icons'
-import {
-  Center,
-  Divider,
-  Grid,
-  GridItem,
-  List,
-  ListItem,
-  Stack
-} from '@chakra-ui/layout'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { SearchIcon } from '@chakra-ui/icons'
+import { Center, Divider, List, ListItem, Stack } from '@chakra-ui/layout'
 
 const Modal = () => {
   return (
@@ -32,18 +24,17 @@ const Modal = () => {
       justify="center"
       align="center"
       borderRadius="1.5rem"
-      border="1px solid black"
     >
       <Flex justifyContent="space-between" alignItems="center" width="100%">
         <Heading fontSize="28px" fontWeight="600" color="#1C1D21">
           Select Token
         </Heading>
         <Spacer />
-        <SmallCloseIcon />
+        <AiOutlineCloseCircle />
       </Flex>
 
       <Box alignItems="center" width="100%" paddingTop="24px">
-        <InputGroup>
+        <InputGroup boxShadow="0px 12px 54px rgba(46, 46, 46, 0.1)">
           <InputLeftElement
             pointerEvents="none"
             // eslint-disable-next-line react/no-children-prop
@@ -53,40 +44,69 @@ const Modal = () => {
         </InputGroup>
       </Box>
 
-      <Stack direction="row" spacing={4} alignItems="center" w="100%" p="24px">
+      <Stack direction="row" spacing={4} alignItems="center" w="100%">
         <Button
-          leftIcon={<EmailIcon />}
+          my="30px"
+          leftIcon={
+            <Avatar
+              size="xs"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+          }
           borderRadius="200px"
           color="white"
           backgroundColor="black"
           hover="none"
+          fontSize="16px"
         >
           WMATIC
         </Button>
         <Button
-          leftIcon={<EmailIcon />}
+          leftIcon={
+            <Avatar
+              size="xs"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+          }
           borderRadius="200px"
           color="white"
           backgroundColor="black"
           hover="none"
+          fontSize="16px"
         >
           USDT
         </Button>
         <Button
-          leftIcon={<EmailIcon />}
+          leftIcon={
+            <Avatar
+              size="xs"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+          }
           borderRadius="200px"
           color="white"
           backgroundColor="black"
           hover="none"
+          fontSize="16px"
         >
           WETH
         </Button>
         <Button
-          leftIcon={<EmailIcon />}
+          leftIcon={
+            <Avatar
+              size="xs"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+          }
           borderRadius="200px"
           color="white"
           backgroundColor="black"
           hover="none"
+          fontSize="16px"
         >
           BNB
         </Button>
@@ -107,6 +127,68 @@ const Modal = () => {
         >
           <Flex align="center" gridGap="10px">
             <Text>1</Text>
+            <Avatar
+              size="sm"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+            <Box d="flex" flexDirection="column">
+              <Text fontSize="16px" fontWeight="600" color="#1C1D21">
+                ETH
+              </Text>
+              <Text fontSize="14px" fontWeight="600" color="#1C1D21">
+                Ethereum
+              </Text>
+            </Box>
+          </Flex>
+
+          <Text fontSize="16px" fontWeight="600" color="##1C1D21">
+            1,683
+          </Text>
+        </ListItem>
+        <Center h="20px" justifyContent="end">
+          <Divider w="84.5%" style={{ border: '1px solid #1C1D21 60%' }} />
+        </Center>
+
+        <ListItem
+          d="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          margin="12px 0 8px 0"
+        >
+          <Flex align="center" gridGap="10px">
+            <Text>2</Text>
+            <Avatar
+              size="sm"
+              name="Dan Abrahmov"
+              src="https://thispersondoesnotexist.com/image"
+            />
+            <Box d="flex" flexDirection="column">
+              <Text fontSize="16px" fontWeight="600" color="#1C1D21">
+                ETH
+              </Text>
+              <Text fontSize="14px" fontWeight="600" color="#1C1D21">
+                Ethereum
+              </Text>
+            </Box>
+          </Flex>
+
+          <Text fontSize="16px" fontWeight="600" color="##1C1D21">
+            1,683
+          </Text>
+        </ListItem>
+        <Center h="20px" justifyContent="end">
+          <Divider w="84.5%" style={{ border: '1px solid #1C1D21 60%' }} />
+        </Center>
+
+        <ListItem
+          d="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          margin="12px 0 8px 0"
+        >
+          <Flex align="center" gridGap="10px">
+            <Text>3</Text>
             <Avatar
               size="sm"
               name="Dan Abrahmov"
