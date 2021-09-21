e394ecfe53e3623a939a203b0a7d08be0d386614
Lidiane Faria <lidianerdefaria@gmail.com>
2021-09-21 17:52:14 -0300

    style: home styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import NavBar from "../components/NavBar";
-import { Hero } from "../components/Hero";
-import { Container } from "../components/Container";
-import { Main } from "../components/Main";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
-import { SwapNavigate } from "../components/SwapNavigate";
-import { MoralisProvider } from "react-moralis";
-import { MORALIS_APP_ID, MORALIS_SERVER_URL } from "../shared/constants";
+import NavBar from '../components/NavBar'
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { SwapNavigate } from '../components/SwapNavigate'
+import { MoralisProvider } from 'react-moralis'
+import { MORALIS_APP_ID, MORALIS_SERVER_URL } from '../shared/constants'
 import {
   Flex,
   Heading,
@@ -13,18 +13,18 @@ import {
   VStack,
   Divider,
   Box,
-  Grid,
-} from "@chakra-ui/react";
+  Grid
+} from '@chakra-ui/react'
 
 const Index = () => (
   <MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
     <Container height="100vh">
       <NavBar />
 
-      <Flex height="100vh" align='center'>
-        <VStack w='50%' p={10} spacing={10} alignItems="flex-start">
+      <Flex height="100vh" align="center" px="5%" gridGap={85}>
+        <VStack w="50%" spacing={8} alignItems="flex-start">
           <Heading as="h1" size="lg" fontSize="64px" fontWeight="600">
-            Sushi swap
+            Sushiswap
           </Heading>
           <Text fontSize="20px" fontWeight="400" color="rgba(0,0,0,0.6)">
             Be a DeFi Chef with Sushi. Swap, earn, stack yields, lend, borrow,
@@ -35,7 +35,7 @@ const Index = () => (
             orientation="horizontal"
             border="0.6px solid rgba(0, 0, 0, 0.2)"
           />
-          <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+          <Grid templateColumns="repeat(4, 4fr)" gap={6}>
             <Box
               d="flex"
               flexDirection="column"
@@ -82,7 +82,7 @@ const Index = () => (
               gridGap={4}
             >
               <Text fontSize="24px" fontWeight="600" color="#E60B8B">
-                1,863
+                1.863
               </Text>
               <Text fontSize="14px" color="rgba(0,0,0,0.6)">
                 Total Pairs
@@ -90,12 +90,12 @@ const Index = () => (
             </Box>
           </Grid>
         </VStack>
-        <VStack w='50%'>
+        <VStack w="50%">
           <SwapNavigate />
         </VStack>
       </Flex>
     </Container>
   </MoralisProvider>
-);
+)
 
-export default Index;
+export default Index
